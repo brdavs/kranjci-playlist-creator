6ef5eaf92e3c559d99ff451d98f3e5e659c85e51
Toni Anzlovar <toni@formalibre.si>
2016-05-05 23:41:20 +0200

    Working update

--- src/js/directives.js
+++ src/js/directives.js
@@ -1,37 +1,74 @@
-'use strict';
-angular.module('KPApp', [])
+/*
+ * Main dirlist directive
+ */
+(function() {
+    'use strict';
 
-.directive('dirlist', ['$location', '$http', function($location, $http) {
-    
-    var list_url = 'http://localhost:8000/list';
-    var playlist_url = 'http://localhost:8000/playlist';
+    angular
+        .module('KPApp')
+        .directive('dirlist', Directive);
 
-    var link = function(scope, element, attrs) {
-        $http.get(list_url).then(function(res) {
-            scope.playlist = [];
-            scope.list = res.data;
-            scope.current_path = scope.list.path;
-            scope.items = scope.list.children;
-            scope.current_items = scope.list.children;
-        });
-        
-        scope.chdir = function(item) {
-            scope.current_items = item.children;
-            console.log(item.path)
+    Directive.$inject = ['$location', '$http'];
+    function Directive($location, $http) {
+        // Usage:
+        //
+        // Creates:
+        //
+        var list_url = 'http://'+$location.$$host+':8000/list';
+        var playlist_url = 'http://'+$location.$$host+':8000/playlist';
+
+        var directive = {
+            bindToController: true,
+            controller: ControllerController,
+            controllerAs: 'vm',
+            link: link,
+            templateUrl: '_dirlist.html',
+            restrict: 'C',
+            scope: {
+            }
         };
+        return directive;
         
-        scope.select = function(item) {
-            scope.playlist.push(item.path);
-            $http.post(playlist_url, scope.playlist).then(function(res) {
-                if (res.data.response != true) scope.playlist = [];
-                console.log(scope.playlist)
+        function link(scope, element, attrs) {
+            $http.get(list_url).then(function(res) {
+                scope.playlist = [];
+                scope.list = res.data;
+                scope.current_path = scope.list.path;
+                scope.items = scope.list.children;
+                scope.current_items = scope.list.children;
             });
+            
+            scope.l = function(x) { console.log(x); }
+            
+            scope.only_name = function(item) {
+                return item.slice(0, -4);
+            }
+            
+            scope.chdir = function(item) {
+                scope.current_items = item.children;
+            };
+            
+            // Fill up the playlist and push it to server
+            scope.select = function(item) {
+                
+                // Push or delete from playlist
+                var position = scope.playlist.indexOf(item.path);
+                if (position==-1) {
+                    scope.playlist.push(item.path);
+                } else {
+                    scope.playlist.splice(position, 1);
+                }
+                console.log(scope.playlist)
+                    
+                $http.post(playlist_url, scope.playlist).then(function(res) {
+                    if (res.data.response != true) scope.playlist = [];
+                });
+                
+            }
         }
+    }
+    /* @ngInject */
+    function ControllerController () {
         
     }
-    
-    return {
-        link: link,
-        templateUrl: '_dirlist.html'
-    }
-}])
+})();
---
+++ src/js/directives_buttons.js
@@ -0,0 +1,60 @@
+/*
+ * Main menu directory button
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('KPApp')
+        .directive('menuLink', Directive);
+
+    Directive.$inject = [];
+    function Directive() {
+        // Usage:
+        //
+        // Creates:
+        //
+        var directive = {
+            bindToController: true,
+            controller: ControllerController,
+            controllerAs: 'vm',
+            link: link,
+            restrict: 'C',
+            scope: {
+            }
+        };
+        return directive;
+        
+        function link(scope, element, attrs) {
+
+            element.bind('click', function() {
+                // reset all active menu items
+                $('.k-list span.menu-link .folder').each(function(el) {
+                    el = angular.element(el);
+                    el.removeClass('fa-folder-open-o');
+                    el.addClass('fa-folder-o');
+                });
+                // reset all arrows
+                $('.k-list span.menu-link .arrow').each(function(el) {
+                    el = angular.element(el);
+                    el.removeClass('fa-chevron-right');
+                });
+                // Get this particular button in shape
+                var children = element.children();
+                angular.forEach(children, function(el) {
+                    el = angular.element(el);
+                    if (el.hasClass('folder')) el.addClass('fa-folder-open-o');
+                    if (el.hasClass('arrow')) el.addClass('fa-chevron-right');
+                });
+                // Make submenu visible or invisible
+                var submenu = element.parent().find('ul').eq(0);
+                submenu.hasClass('hidden') ? submenu.removeClass('hidden') : submenu.addClass('hidden');
+            });
+            
+        }
+    }
+    /* @ngInject */
+    function ControllerController () {
+        
+    }
+})();
